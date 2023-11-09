ec06a0b51587dcdea2e1dbef018ace64a6f367b5
RicardoPLins <79940823+RicardoPLins@users.noreply.github.com>
2023-11-08 23:12:08 -0300

    Delete pweb2-colegiado-dev-ricardo directory

--- pweb2-colegiado-dev-ricardo/pweb2-colegiado-dev-ricardo/pweb2-colegiado-main/venus/src/main/java/br/edu/ifpb/pweb2/venus/model/Assunto.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.pweb2.venus.model;
-
-public class Assunto {
-    //@Id
-    //@GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer id;
-
-    private String nome;
-}
